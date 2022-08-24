923de3c70ca0f132afcc280b04bd4a6c1fb810c6
Anand Kumar <anand.kva00@gmail.com>
2022-08-24 18:15:40 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import BottomNav from "./Pages/BottomNav";
 import Header from "./Pages/Header";
 import About from "./Pages/About";
 import React, { useState, createContext, useEffect } from "react";
+import { Alert } from "react-native";
 
 
 export const UserContext = createContext();
 const App = () => {
-  const [user, setUser] = useState("Anand");
+  const [user, setUser] = useState([]);
+
+  useEffect(()=>{
+    Alert.alert("hello")
+  })
 
 
   return (
